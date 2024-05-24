9353bfe6607a347b6f7152284aaaaa2e4d31fd87
JhordanLuyo <jhordan271992@gmail.com>
2024-05-24 09:39:46 -0500

    jhordanLuyo
    realice algunas actualizaciones

--- Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/kotlin/JhordanLuyo.kt
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/kotlin/JhordanLuyo.kt
@@ -107,7 +107,11 @@ fun main(){
     println("""
     DIFICULTAD EXTRA (opcional):
  * Crea un programa que imprima por consola todos los números comprendidos
+<<<<<<< Updated upstream
  * entre 10 y 55 (incluidos), pares, y que no son ni el 16 ni múltiplos de 3 1.
+=======
+ * entre 10 y 55 (incluidos), pares, y que no son ni el 16 ni múltiplos de 3.
+>>>>>>> Stashed changes
     """)
     for (i in 10..55){
         if((i != 16 && i%3 != 0) && i%2 == 0){
